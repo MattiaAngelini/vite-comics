98144c081f92610e6483a222267f80e62a415d55
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-12 16:28:10 +0200

    Add: elements header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,28 @@
 </script>
 
 <template>
-  header
+   
+   <section class="container d-flex justify-content-between align-items-center">
+    
+    <div>
+        <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+
+    <ul class="d-flex gap-3">
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>CHARACTERS</li>
+        <li>TV</li>
+        <li>CHARACTERS</li>
+        <li>GAMES</li>
+        <li>CHARACTERS</li>
+        <li>VIDEO</li>
+        <li>CHARACTERS</li>
+    </ul>
+
+   </section>
+
 
 </template>
 
