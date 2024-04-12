6b5a2e751bb49d08ecc79db3ce5fe9f6e052821a
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-12 18:54:01 +0200

    Add: wraps footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,16 +7,44 @@
 
 <template>
 
+  <section class="top-footer">
 
-  <section class="signup-footer">footer</section>
+      <div class="ms-container">
+       prova
+    
+     </div>
+
+
+  </section>
   
-
+  <section class="bottom-footer">footer</section>
+  
 </template>
+
+
+
 
 <style scoped lang="scss">
  @use '../style/partials/variables' as *;
 
- .signup-footer {
+
+
+ .top-footer {
+  background-image: url('../assets/img/footer-bg.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: white;
+
+  .ms-container {
+    height: 300px;
+    background-image: url('../assets/img/dc-logo-bg.png');
+    background-repeat: no-repeat;
+    background-position: 100%;
+    background-size: contain;
+  }
+ }
+
+ .bottom-footer {
   background-color: $brand-secondary;
  }
 
